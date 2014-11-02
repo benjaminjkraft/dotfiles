0efbf96b55137d6754215311ff90e60bd2e94b17
Ben Kraft <benjaminjkraft@gmail.com>
2014-11-02 15:18:52 -0500

    Fixes for install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 #run with wget -O - <url> | bash
 
+set +e
+
 exclude="README.md .git LICENSE"
 dir="${1:-.dotfiles}"
 
 cd "$HOME"
 git clone git@github.com:benjaminjkraft/dotfiles.git "$dir"
 # A little bit fragile, but it works.
-files=$(la "$dir" | grep -v "^$(echo $exclude | sed 's/ /\$\\|\^/g')$")
+files=$(ls -A "$dir" | grep -v "^$(echo $exclude | sed 's/ /\$\\|\^/g')$")
 
 for i in files ; do
   ln -s "$dir/$i"
+done
