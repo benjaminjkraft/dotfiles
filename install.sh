057f76b13ebdb6ed9a2c6cfa277c0daf15d36043
Ben Kraft <benjaminjkraft@gmail.com>
2014-11-02 15:30:17 -0500

    Fix to install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ git clone https://github.com/benjaminjkraft/dotfiles.git "$dir"
 # A little bit fragile, but it works.
 files=$(ls -A "$dir" | grep -v "^$(echo $exclude | sed 's/ /\$\\|\^/g')$")
 
-for i in files ; do
+for i in $files ; do
   ln -s "$dir/$i"
 done
