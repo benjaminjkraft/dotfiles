42e44fc04b09cabcb89bbf52d492675fb5578e5a
Ben Kraft <benjaminjkraft@gmail.com>
2014-11-02 15:39:51 -0500

    Add rm -rf to install

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ exclude="README.md .git LICENSE"
 dir="${1:-.dotfiles}"
 
 cd "$HOME"
-rm -Ir "$dir"
+rm -rf "$dir"
 git clone https://github.com/benjaminjkraft/dotfiles.git "$dir"
 # A little bit fragile, but it works.
 files=$(ls -A "$dir" | grep -v "^$(echo $exclude | sed 's/ /\$\\|\^/g')$")
 
 for i in $files ; do
-  ln -si "$dir/$i"
+  ln -sf "$dir/$i"
 done
